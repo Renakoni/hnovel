a97c24e7b86c1f13e89180daf18d04448b5fbe4c
SecPan <qq2579937786@gmail.com>
2026-09-07 06:18:22 -0700

    fix(reader): avoid top cutout inset in bottom bar

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/book/reader/ReaderScreen.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/book/reader/ReaderScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsIgnoringVisibility
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsIgnoringVisibility
@@ -560,7 +561,7 @@ private fun BottomBar(
     onClickChapterSelector: () -> Unit
 ) {
     BottomAppBar(
-        windowInsets = WindowInsets.systemBarsIgnoringVisibility
+        windowInsets = WindowInsets.navigationBarsIgnoringVisibility
     ) {
         Row(
             Modifier
