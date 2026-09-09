2666b4addde58e25493fd8d119d0da2e4020735c
SecPan <qq2579937786@gmail.com>
2026-09-09 13:43:27 -0700

    fix(rules): use valid newline token in script scanner

--- source-rules/src/main/kotlin/hnovel/rules/RuleParser.kt
+++ source-rules/src/main/kotlin/hnovel/rules/RuleParser.kt
@@ -93,7 +93,7 @@ class RuleParser {
             when {
                 text.regionMatches(index, "</js>", 0, 5, true) -> return index
                 text[index] in "\"'`" -> index = skipUnit(text, index, location, budget)
-                text.startsWith("//", index) -> index = text.indexOf('\\n', index).takeIf { it >= 0 } ?: text.length
+                text.startsWith("//", index) -> index = text.indexOf('\n', index).takeIf { it >= 0 } ?: text.length
                 text.startsWith("/*", index) -> index = text.indexOf("*/", index + 2).takeIf { it >= 0 }?.plus(2)
                     ?: fail(location, index, "UnclosedComment")
                 text[index] == '/' && previous in "=([{,:;!?&|" -> {
